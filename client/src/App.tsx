0dc5d6c4630c89850d8e33a67f6c5b54a1d02e7d
REMI DUBUS <remdubus@gmail.com>
2024-11-07 17:07:15 +0100

    feat: add media queries to home page s01us39

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -5,15 +5,11 @@ import "./App.css";
 function App() {
   return (
     <>
-      <nav>
-        <h1>JE SUIS UNE NAVBAR</h1>
-      </nav>
+      <nav> </nav>
       <main>
         <Outlet />
       </main>
-      <footer>
-        <h2>JE SUIS UN FOOTER</h2>
-      </footer>
+      <footer> </footer>
     </>
   );
 }
