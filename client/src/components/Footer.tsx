dc4fcea33c4237c3269f67b1ad0896ce86da717b
Sanesthesie <victorsanlopez@gmail.com>
2024-11-07 18:09:59 +0100

    feat: create footer module css

--- client/src/components/Footer.tsx
+++ client/src/components/Footer.tsx
@@ -1,22 +1,32 @@
+import style from "./Footer.module.css";
+
 export default function Footer() {
   return (
-    <footer>
-      <h3>BOARDER LINE</h3>
-      <img src="https://picsum.photos/200" alt="Logo Boarder Line" />
-      <section>
-        <p>Contacts</p>
-        <p>sloubigang@contact.com</p>
-        <p>01 39 35 XX XX</p>
-      </section>
-      <section>
-        <p>Follow us</p>
-        <ul>
-          <li>
-            <img src="https://picsum.photos/25" alt="Logo LinkedIn" />
-            <img src="https://picsum.photos/25" alt="Logo Instagram" />
-            <img src="https://picsum.photos/25" alt="Logo Vero" />
-          </li>
-        </ul>
+    <footer className={style.footerMain}>
+      <section className={style.section}>
+        <div className={style.footerRight}>
+          <p>Contacts</p>
+          <p>sloubigang@contact.com</p>
+          <p>01 39 35 XX XX</p>
+        </div>
+        <div className={style.footerMiddle}>
+          <h3>BOARDER LINE</h3>
+          <img
+            className={style.logoClass}
+            src="https://picsum.photos/100"
+            alt="Logo Boarder Line"
+          />
+        </div>
+        <div className={style.footerLeft}>
+          <p>Follow us</p>
+          <ul>
+            <li className={style.social}>
+              <img src="https://picsum.photos/25" alt="Logo LinkedIn" />
+              <img src="https://picsum.photos/25" alt="Logo Instagram" />
+              <img src="https://picsum.photos/25" alt="Logo Vero" />
+            </li>
+          </ul>
+        </div>
       </section>
       <section>
         <p>Mentions légales et CGU • Données personnelles et cookies</p>
