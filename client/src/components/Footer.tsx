cf432531441b4d9e80fd597c62bd82e33a82a48d
Sanesthesie <victorsanlopez@gmail.com>
2024-11-07 17:14:08 +0100

    feat: create footer component

---
+++ client/src/components/Footer.tsx
@@ -0,0 +1,26 @@
+export default function Footer() {
+  return (
+    <footer>
+      <h3>BOARDER LINE</h3>
+      <img src="https://picsum.photos/200" alt="Logo Boarder Line" />
+      <section>
+        <p>Contacts</p>
+        <p>sloubigang@contact.com</p>
+        <p>01 39 35 XX XX</p>
+      </section>
+      <section>
+        <p>Follow us</p>
+        <ul>
+          <li>
+            <img src="https://picsum.photos/25" alt="Logo LinkedIn" />
+            <img src="https://picsum.photos/25" alt="Logo Instagram" />
+            <img src="https://picsum.photos/25" alt="Logo Vero" />
+          </li>
+        </ul>
+      </section>
+      <section>
+        <p>Mentions légales et CGU • Données personnelles et cookies</p>
+      </section>
+    </footer>
+  );
+}
