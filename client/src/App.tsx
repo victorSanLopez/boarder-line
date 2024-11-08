752022e14ce75118f941e9466c73c42688490abb
Amandine GRARD-LAURENT <a.grard.laurent08@gmail.com>
2024-11-08 19:13:52 +0100

    feat: navbar closes s01us01

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -2,7 +2,7 @@ import { Outlet } from "react-router-dom";
 import "./App.css";
 import Footer from "./components/footer/Footer";
 import HeroHeader from "./components/heroHeader/HeroHeader";
-import Navbar from "./components/toto/Navbar";
+import Navbar from "./components/navbar/Navbar";
 
 function App() {
   return (
