684e9262e0b70f8eb30a6d4f50d578a23c93f27d
LEFEVER JEAN-PHILIPPE <lefever.jp@hotmail.fr>
2024-11-10 20:18:17 +0100

    feat: add responsive

--- client/src/pages/HomePage/HomePage.tsx
+++ client/src/pages/HomePage/HomePage.tsx
@@ -1,6 +1,6 @@
-import style from "./HomePage.module.css";
 import { useState } from "react";
 import FilterPanel from "../../components/filterPanel/FilterPanel";
+import style from "./HomePage.module.css";
 
 export default function HomePage() {
   const [filter, setFilter] = useState("1");
