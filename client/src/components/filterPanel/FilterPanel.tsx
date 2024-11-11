5c97f1b8352a213fc9b803a2b974e34b707b1108
LEFEVER JEAN-PHILIPPE <lefever.jp@hotmail.fr>
2024-11-11 15:06:05 +0100

    feat: all filter ok

--- client/src/components/filterPanel/FilterPanel.tsx
+++ client/src/components/filterPanel/FilterPanel.tsx
@@ -2,7 +2,7 @@ import { useState } from "react";
 import type { filterPanelProps } from "../../assets/lib/definition";
 import style from "./filterPanel.module.css";
 
-function FilterPanel({ filter, setFilter }: filterPanelProps) {
+function FilterPanel({ setFilter }: filterPanelProps) {
   const [displayTime, setDisplayTime] = useState(false);
   const [displayRating, setDisplayRating] = useState(false);
   const [displayType, setDisplayType] = useState(false);
@@ -15,8 +15,11 @@ function FilterPanel({ filter, setFilter }: filterPanelProps) {
     displayRating ? setDisplayRating(false) : setDisplayRating(true);
   const handleClickType = () =>
     displayType ? setDisplayType(false) : setDisplayType(true);
+
   const handleChangeFilter = (e: React.ChangeEvent<HTMLInputElement>) => {
-    setFilter(e.target.value);
+    if (e.target.checked) {
+      setFilter(e.target.value);
+    } else setFilter("");
   };
 
   return (
@@ -37,21 +40,20 @@ function FilterPanel({ filter, setFilter }: filterPanelProps) {
                 <label className={style.label} htmlFor="maxPlayer1">
                   <input
                     type="checkbox"
-                    id="maxPlayer1"
-                    name="player"
+                    id="minPlayer1"
+                    name="minPlayers"
                     value="1"
-                    checked={filter === "1"}
                     onChange={handleChangeFilter}
                   />
-                  1 player{" "}
+                  1 player
                 </label>
+
                 <label className={style.label} htmlFor="maxPlayer2">
                   <input
                     type="checkbox"
-                    id="maxPlayer2"
-                    name="player"
+                    id="minPlayer2"
+                    name="minPlayers"
                     value="2"
-                    checked={filter === "2"}
                     onChange={handleChangeFilter}
                   />
                   2 players
@@ -59,10 +61,9 @@ function FilterPanel({ filter, setFilter }: filterPanelProps) {
                 <label className={style.label} htmlFor="maxPlayer3">
                   <input
                     type="checkbox"
-                    id="maxPlayer3"
-                    name="player"
+                    id="minPlayer3"
+                    name="minPlayers"
                     value="3"
-                    checked={filter === "3"}
                     onChange={handleChangeFilter}
                   />
                   3 players
@@ -70,13 +71,12 @@ function FilterPanel({ filter, setFilter }: filterPanelProps) {
                 <label className={style.label} htmlFor="maxPlayer4">
                   <input
                     type="checkbox"
-                    id="maxPlayer4"
-                    name="player"
+                    id="minPlayer4"
+                    name="minPlayers"
                     value="4"
-                    checked={filter === "4"}
                     onChange={handleChangeFilter}
                   />
-                  4 players+
+                  4 players
                 </label>
               </fieldset>
             )}
@@ -100,7 +100,6 @@ function FilterPanel({ filter, setFilter }: filterPanelProps) {
                     id="time1"
                     name="time"
                     value="30"
-                    checked={filter === "30"}
                     onChange={handleChangeFilter}
                   />
                   30 min
@@ -111,7 +110,6 @@ function FilterPanel({ filter, setFilter }: filterPanelProps) {
                     id="time2"
                     name="time"
                     value="60"
-                    checked={filter === "60"}
                     onChange={handleChangeFilter}
                   />
                   60 min{" "}
@@ -122,7 +120,6 @@ function FilterPanel({ filter, setFilter }: filterPanelProps) {
                     id="time3"
                     name="time"
                     value="90"
-                    checked={filter === "90"}
                     onChange={handleChangeFilter}
                   />
                   90 min{" "}
@@ -133,7 +130,6 @@ function FilterPanel({ filter, setFilter }: filterPanelProps) {
                     id="time4"
                     name="time"
                     value="120"
-                    checked={filter === "120"}
                     onChange={handleChangeFilter}
                   />
                   120 min+
@@ -154,123 +150,62 @@ function FilterPanel({ filter, setFilter }: filterPanelProps) {
           <div>
             {displayRating && (
               <fieldset className={style.fieldset}>
-                <label className={style.label} htmlFor="rating0">
-                  <input
-                    type="radio"
-                    id="rating0"
-                    name="rating"
-                    value="0.0"
-                    checked={filter === "0.0"}
-                    onChange={handleChangeFilter}
-                  />
-                  0
-                </label>
-                <label className={style.label} htmlFor="rating1">
-                  <input
-                    type="radio"
-                    id="rating1"
-                    name="rating"
-                    value="1.0"
-                    checked={filter === "1.0"}
-                    onChange={handleChangeFilter}
-                  />
-                  1{" "}
-                </label>
-                <label className={style.label} htmlFor="rating3">
-                  <input
-                    type="radio"
-                    id="rating3"
-                    name="rating"
-                    value="2.0"
-                    checked={filter === "2.0"}
-                    onChange={handleChangeFilter}
-                  />
-                  2
-                </label>
-                <label className={style.label} htmlFor="rating3">
-                  <input
-                    type="radio"
-                    id="rating3"
-                    name="rating"
-                    value="3.0"
-                    checked={filter === "3.0"}
-                    onChange={handleChangeFilter}
-                  />
-                  3
-                </label>
-                <label className={style.label} htmlFor="rating4">
-                  <input
-                    type="radio"
-                    id="rating4"
-                    name="rating"
-                    value="4.0"
-                    checked={filter === "4.0"}
-                    onChange={handleChangeFilter}
-                  />
-                  4
-                </label>
                 <label className={style.label} htmlFor="rating5">
                   <input
-                    type="radio"
+                    type="checkbox"
                     id="rating5"
                     name="rating"
                     value="5.0"
-                    checked={filter === "5.0"}
                     onChange={handleChangeFilter}
                   />
-                  5{" "}
+                  5
                 </label>
                 <label className={style.label} htmlFor="rating6">
                   <input
-                    type="radio"
+                    type="checkbox"
                     id="rating6"
                     name="rating"
                     value="6.0"
-                    checked={filter === "6.0"}
                     onChange={handleChangeFilter}
                   />
                   6
                 </label>
                 <label className={style.label} htmlFor="rating7">
                   <input
-                    type="radio"
+                    type="checkbox"
                     id="rating7"
                     name="rating"
                     value="7.0"
-                    checked={filter === "7.0"}
                     onChange={handleChangeFilter}
                   />
                   7
                 </label>
                 <label className={style.label} htmlFor="rating8">
                   <input
-                    type="radio"
+                    type="checkbox"
                     id="rating8"
                     name="rating"
                     value="8.0"
-                    checked={filter === "8.0"}
                     onChange={handleChangeFilter}
                   />
                   8{" "}
                 </label>
                 <label className={style.label} htmlFor="rating9">
                   <input
-                    type="radio"
+                    type="checkbox"
                     id="rating9"
                     name="rating"
                     value="9.0"
-                    checked={filter === "9.0"}
                     onChange={handleChangeFilter}
                   />
                   9
                 </label>
                 <label className={style.label} htmlFor="rating10">
                   <input
-                    type="radio"
+                    type="checkbox"
                     id="rating10"
                     name="rating"
                     value="10.0"
-                    checked={filter === "10.0"}
                     onChange={handleChangeFilter}
                   />
                   10
@@ -293,44 +228,40 @@ function FilterPanel({ filter, setFilter }: filterPanelProps) {
               <fieldset className={style.fieldset}>
                 <label className={style.label} htmlFor="type1">
                   <input
-                    type="radio"
+                    type="checkbox"
                     id="type1"
                     name="type"
                     value="Multi-Use Cards"
-                    checked={filter === "Multi-Use Cards"}
                     onChange={handleChangeFilter}
                   />
                   Deck builder
                 </label>
                 <label className={style.label} htmlFor="type2">
                   <input
-                    type="radio"
+                    type="checkbox"
                     id="type2"
                     name="type"
                     value="Dice Rolling"
-                    checked={filter === "Dice Rolling"}
                     onChange={handleChangeFilter}
                   />
                   Dice
                 </label>
                 <label className={style.label} htmlFor="type3">
                   <input
-                    type="radio"
+                    type="checkbox"
                     id="type3"
                     name="type"
                     value="Cooperative Game"
-                    checked={filter === "Cooperative Game"}
                     onChange={handleChangeFilter}
                   />
                   Cooperative
                 </label>
                 <label className={style.label} htmlFor="type4">
                   <input
-                    type="radio"
+                    type="checkbox"
                     id="type4"
                     name="type"
                     value="Automatic Resource Growth"
-                    checked={filter === "Automatic Resource Growth"}
                     onChange={handleChangeFilter}
                   />
                   Ressource management
@@ -339,9 +270,6 @@ function FilterPanel({ filter, setFilter }: filterPanelProps) {
             )}
           </div>
         </div>
-        <button className={style.button} type="button">
-          All games
-        </button>
       </section>
     </>
   );
--- client/src/pages/HomePage/HomePage.tsx
+++ client/src/pages/HomePage/HomePage.tsx
@@ -7,23 +7,69 @@ export default function HomePage() {
   const [filter, setFilter] = useState("");
   const fullGame = gameData;
 
-  const userChoice =
-    filter === ""
-      ? fullGame
-      : fullGame.filter((f) => f.minPlayers === Number.parseInt(filter));
+  const userFilterPlayers = fullGame.filter(
+    (f) => f.minPlayers === Number.parseInt(filter),
+  );
+  const userFilterTime = fullGame.filter(
+    (f) => f.playingTime === Number.parseInt(filter),
+  );
+  const userFilterRating = fullGame.filter(
+    (f) => f.rating === Number.parseFloat(filter),
+  );
+  const userFilterType = fullGame.filter((f) => f.mechanics?.includes(filter));
 
   return (
     <section className={style.backgroundPage}>
       <FilterPanel filter={filter} setFilter={setFilter} />
       <div className={style.myDiv}>
-        {userChoice.map((g) => (
-          <div key={g.gameId} className={style.div}>
-            <h1>{g.name}</h1>
-            <img className={style.img} src={g.image} alt={g.name} />
-            <p>{g.minPlayers}</p>
-            <p>{g.playingTime}</p> <p>{g.rating}</p>
-          </div>
-        ))}
+        {Number.parseInt(filter) < 10 &&
+          userFilterPlayers.map((g) => (
+            <div key={g.gameId} className={style.div}>
+              <h1>{g.name}</h1>
+              <img className={style.img} src={g.image} alt={g.name} />
+              <p>{g.minPlayers}</p>
+              <p>{g.playingTime}</p> <p>{g.rating}</p>
+            </div>
+          ))}
+        {Number.parseInt(filter) > 10 &&
+          userFilterTime.map((g) => (
+            <div key={g.gameId} className={style.div}>
+              <h1>{g.name}</h1>
+              <img className={style.img} src={g.image} alt={g.name} />
+              <p>{g.minPlayers}</p>
+              <p>{g.playingTime}</p> <p>{g.rating}</p>
+            </div>
+          ))}
+        {Number.parseFloat(filter) >= 5.0 &&
+          userFilterRating.map((g) => (
+            <div key={g.gameId} className={style.div}>
+              <h1>{g.name}</h1>
+              <img className={style.img} src={g.image} alt={g.name} />
+              <p>{g.minPlayers}</p>
+              <p>{g.playingTime}</p> <p>{g.rating}</p>
+            </div>
+          ))}
+        {(filter === "Dice Rolling" ||
+          filter === "Automatic Resource Growth" ||
+          filter === "Cooperative Game" ||
+          filter === "Multi-Use Cards") &&
+          userFilterType.map((g) => (
+            <div key={g.gameId} className={style.div}>
+              <h1>{g.name}</h1>
+              <img className={style.img} src={g.image} alt={g.name} />
+              <p>{g.minPlayers}</p>
+              <p>{g.playingTime}</p> <p>{g.rating}</p>
+            </div>
+          ))}
+        {filter === "" &&
+          fullGame.map((g) => (
+            <div key={g.gameId} className={style.div}>
+              <h1>{g.name}</h1>
+              <img className={style.img} src={g.image} alt={g.name} />
+              <p>{g.minPlayers}</p>
+              <p>{g.playingTime}</p> <p>{g.rating}</p>
+            </div>
+          ))}
       </div>
     </section>
   );
