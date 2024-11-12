9d87d5af9a1eb7efa26369a0aadc95aa8ffdd822
Amandine GRARD-LAURENT <a.grard.laurent08@gmail.com>
2024-11-12 16:26:06 +0100

    feat: s02us40 library page

--- client/src/pages/libraryPage/LibraryPage.tsx
+++ client/src/pages/libraryPage/LibraryPage.tsx
@@ -1,5 +1,7 @@
 import style from "./libraryPage.module.css";
 
+// page library pour lister les jeux et les filtrer
+
 export default function LibraryPage() {
   return <section className={style.backgroundPage}> </section>;
 }
