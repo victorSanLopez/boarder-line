0862f287f8e527e40c448611a16f7632e3e22fcf
Amandine GRARD-LAURENT <a.grard.laurent08@gmail.com>
2024-11-12 13:31:47 +0100

    feat: s02us37_cardGame closes

--- client/src/pages/homePage/HomePage.tsx
+++ client/src/pages/homePage/HomePage.tsx
@@ -1,21 +1,5 @@
-import CardGame from "../../components/cardGame/CardGame";
 import style from "./homePage.module.css";
 
-const cardList = [
-  {
-    name: "10 Days in Africa",
-    image:
-      "https://cf.geekdo-images.com/B1Bji6N6r1kUvI_Glp7U1w__original/img/3Y_svR8FUmL-yicbCq3bwBvSYKc=/0x0/filters:format(jpeg)/pic1229634.jpg",
-    rating: 0,
-    want: false,
-    yearPublished: 2023,
-  },
-];
-
 export default function HomePage() {
-  return (
-    <section className={style.backgroundPage}>
-      <CardGame card={cardList[0]} />
-    </section>
-  );
+  return <section className={style.backgroundPage}> </section>;
 }
