297df050eccb40f758f672dc67eb5fb86e6583e2
LEFEVER JEAN-PHILIPPE <lefever.jp@hotmail.fr>
2024-11-13 09:38:31 +0100

    feat: add cardGame component

--- client/src/assets/lib/definition.tsx
+++ client/src/assets/lib/definition.tsx
@@ -3,7 +3,6 @@ export type headerInfoProps = {
   subtitle: string;
   description: string;
 };
-<<<<<<< HEAD
 export type filterPanelProps = {
   filterGame: string;
   setFilterGame: (f: string) => void;
@@ -12,11 +11,9 @@ export type dataGames = {
   minPlayers: number;
   rating: number;
   playingTime: number;
-=======
-
+};
 export type cardProps = {
   name: string;
   image: string;
   rating: number;
->>>>>>> d85e12fec501ee1d6862e4fc17dd9c20374a47d9
 };
