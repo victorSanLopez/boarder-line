2d29e7feaef9ba4b8327d1a0417ad3c8f88dae9e
Amandine GRARD-LAURENT <a.grard.laurent08@gmail.com>
2024-11-27 17:47:20 +0100

    feat: click sur le coeur pour supprimer le jeu des favoris

--- client/src/components/cardGame/CardGame.tsx
+++ client/src/components/cardGame/CardGame.tsx
@@ -7,12 +7,14 @@ function CardGame({
   rating,
   image,
   handleClickFavorite,
+  handleClickFavoriteOff,
 }: {
   gameId: number;
   name: string;
   rating: number;
   image: string;
   handleClickFavorite(id: string): void;
+  handleClickFavoriteOff(id: string): void;
 }) {
   // je récupère les favoris des jeux sur toutes les pages
   const listedFavorites = JSON.parse(
@@ -39,27 +41,55 @@ function CardGame({
         </NavLink>
         <h2 className={style.title}>Name : {nameSlice}</h2>
         <h2 className={style.title}>Rating : {rating.toFixed(0)}/10</h2>
-        <button
-          type="button"
-          className={style.button}
-          onClick={() => {
-            handleClickFavorite(gameId.toString());
-          }}
-        >
-          <svg
-            className={
-              listedFavorites.includes(gameId) ? style.heartOn : style.heartOff
-            }
-            viewBox="0 0 32 29.6"
+        {listedFavorites.includes(gameId) ? (
+          <button
+            type="button"
+            className={style.button}
+            onClick={() => {
+              handleClickFavoriteOff(gameId.toString());
+            }}
           >
-            <title>heart shape</title>
-            <path
-              d="M23.6,0c-3.4,0-6.3,2.7-7.6,5.6C14.7,2.7,11.8,0,8.4,0C3.8,0,0,3.8,0,8.4c0,9.4,9.5,11.9,16,21.2
+            <svg
+              className={
+                listedFavorites.includes(gameId)
+                  ? style.heartOn
+                  : style.heartOff
+              }
+              viewBox="0 0 32 29.6"
+            >
+              <title>heart shape</title>
+              <path
+                d="M23.6,0c-3.4,0-6.3,2.7-7.6,5.6C14.7,2.7,11.8,0,8.4,0C3.8,0,0,3.8,0,8.4c0,9.4,9.5,11.9,16,21.2
 	c6.1-9.3,16-12.1,16-21.2C32,3.8,28.2,0,23.6,0z"
-            />
-            {/* Coeur en vectoriel */}
-          </svg>
-        </button>
+              />
+              {/* Coeur en vectoriel */}
+            </svg>
+          </button>
+        ) : (
+          <button
+            type="button"
+            className={style.button}
+            onClick={() => {
+              handleClickFavorite(gameId.toString());
+            }}
+          >
+            <svg
+              className={
+                listedFavorites.includes(gameId)
+                  ? style.heartOn
+                  : style.heartOff
+              }
+              viewBox="0 0 32 29.6"
+            >
+              <title>heart shape</title>
+              <path
+                d="M23.6,0c-3.4,0-6.3,2.7-7.6,5.6C14.7,2.7,11.8,0,8.4,0C3.8,0,0,3.8,0,8.4c0,9.4,9.5,11.9,16,21.2
+	c6.1-9.3,16-12.1,16-21.2C32,3.8,28.2,0,23.6,0z"
+              />
+              {/* Coeur en vectoriel */}
+            </svg>
+          </button>
+        )}
       </article>
     </section>
   );
